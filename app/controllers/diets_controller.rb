f0cf91dab50c1d22c7e2d92a7b6b6bc93e327b2e
Yoshiki Kondou <yosiking_1234@yahoo.co.jp>
2022-08-23 16:54:49 +0900

    index table作成

--- app/controllers/diets_controller.rb
+++ app/controllers/diets_controller.rb
@@ -4,12 +4,18 @@ class DietsController < ApplicationController
 
   def index
     @diets = Diet.all
+    @search_params = diet_search_params
+    @diets = Diet.search(@search_params)
     @diet_by_day = @diets.group("date(record_time)")
     @chartlabels = @diet_by_day.size.map(&:first).sort { |a,b| a <=> b }.to_json.html_safe
     @proteindata = @diet_by_day.sum("protein * 4").map(&:second).to_json.html_safe
     @fatdata = @diet_by_day.sum("fat * 9").map(&:second).to_json.html_safe
     @carbohydratedata = @diet_by_day.sum("carbohydrate * 4").map(&:second).to_json.html_safe
     @body_weightdata = @diet_by_day.sum(:body_weight).map(&:second).to_json.html_safe
+    @total_calorie = Diet.sum("protein * + fat * 9 + carbohydrate * 4")
+    @protein_index_data = Diet.sum("protein * 4")
+    @fat_index_data = Diet.sum("fat * 9")
+    @carbohydrate_index_data = Diet.sum("carbohydrate * 4")
   end
 
   def new
@@ -20,7 +26,7 @@ class DietsController < ApplicationController
     @diet = Diet.new(diet_params)
     if @diet.save!
       flash[:success] = "体重・カロリーを記録しました"
-      redirect_to("/workouts")
+      redirect_to("/diets")
     else
       flash[:failure] = "体重・カロリーを記録できませんでした"
       render :new, status: :unprocessable_entity
@@ -33,7 +39,7 @@ class DietsController < ApplicationController
   def update
     if @diet.update(diet_params)
       flash[:success] = "体重・カロリーを更新しました"
-      redirect_to("/workouts")
+      redirect_to("/diets")
     else
       flash[:failure] = "体重・カロリーを更新できませんでした"
       render :edit, status: :unprocessable_entity
@@ -43,7 +49,7 @@ class DietsController < ApplicationController
   def destroy
     @diet.destroy
     flash[:success] = "体重・カロリーを削除しました"
-    redirect_to("/workouts")
+    redirect_to("/diets")
   end
 
   private
@@ -63,5 +69,12 @@ class DietsController < ApplicationController
         :fat,
         :carbohydrate
       ).merge(user_id: session[:user_id])
+    end
+
+    def diet_search_params
+      params.fetch(:search, {}).permit(
+        :from_record_time,
+        :to_record_time
+      )
     end
 end
